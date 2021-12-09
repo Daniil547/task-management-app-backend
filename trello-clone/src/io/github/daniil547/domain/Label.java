8a2d8d136e0446e222ff88a53e25648708d529eb
Daniil547 <72149318+Daniil547@users.noreply.github.com>
2021-12-09 19:12:39 +0200

    Add field color to Label

--- trello-clone/src/io/github/daniil547/domain/Label.java
+++ trello-clone/src/io/github/daniil547/domain/Label.java
@@ -2,6 +2,7 @@ package io.github.daniil547.domain;
 
 public class Label {
     private String name;
+    private int color; //int is better than String
 
     /*TODO: accessors*/
     /*TODO: constructor*/
