2bf120f684b47c1bb52eb4bbcf0d8acc35b29fca
Daniil547 <72149318+Daniil547@users.noreply.github.com>
2021-12-17 14:10:03 +0200

    Refactor domain hierarchy: merge NestedPage into Resource

--- trello-clone/src/io/github/daniil547/domain/CardList.java
+++ trello-clone/src/io/github/daniil547/domain/CardList.java
@@ -9,7 +9,7 @@ import java.util.List;
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 @Data
-public class CardList extends NestedPage {
+public class CardList extends Resource {
     private List<Card> cards;
     private Boolean active = true;
 
