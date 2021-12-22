fe74e15dd62a33eb4a160c44315a1ba91429703b
Daniil547 <72149318+Daniil547@users.noreply.github.com>
2021-12-22 21:04:45 +0200

    Add repositories and change service and presentation layers accordingly

--- trello-clone/src/main/java/io/github/daniil547/services/ResourceService.java
+++ trello-clone/src/main/java/io/github/daniil547/services/ResourceService.java
@@ -6,13 +6,13 @@ import io.github.daniil547.domain.Resource;
 import java.time.ZonedDateTime;
 
 public abstract class ResourceService<T extends Resource> extends DomainService<T> {
-    public T create(T inst, Member creator, String title) {
+    public T create(T inst, Member creator, String page_name, String title, String descr) {
         super.create(inst);
         inst.setCreatedWhen(ZonedDateTime.now());
         inst.setCreatedBy(creator);
+        inst.setName(page_name);
         inst.setTitle(title);
-        // create a default name
-        inst.setName(title + inst.getId());
+        inst.setDescription(descr);
         return inst;
     }
 }
