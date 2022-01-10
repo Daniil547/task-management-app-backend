bbcb0bb6be55e0087bffe5ae1a6f4cad686e45ad
Daniil547 <72149318+Daniil547@users.noreply.github.com>
2022-01-10 20:47:09 +0200

    Refactor DB schema
    1) Reflecting changes in the class hierarchy, remove all the columns (those that were present as well as commented out) that were mapped to (now gone) "Resource" class fields:
    - "created_by"
    - "created_when"
    - "last_updated_by"
    - "last_updated_when"
    2) Add "page_title" column to "users" table ("username" and "about" are instead of "page_name" and "page_description")
    3) Remove V18 because applying its changes directly to the previous version (№1)

--- trello-clone/src/main/resources/db/migration/V4__add_workspaces_table.sql
+++ trello-clone/src/main/resources/db/migration/V4__add_workspaces_table.sql
@@ -2,15 +2,6 @@ create table workspaces
 (
     id                  uuid primary key,
 
-    /* replaced by adding "OWNER" option to the Role enum
-    created_by uuid references workspace_members(user_id, workspace_id) not null,
-     */
-    created_when        timestamptz          not null,
-    /* problematic, better be implemented via logging and aspects
-    last_updated_by uuid references workspace_members(user_id, workspace_id),
-    last_updated_when timestamptz,
-    */
-
     page_title          varchar(20)          not null default 'My Workspace',
     page_name           varchar(25)          not null unique,
     page_description    varchar(1000),
--- trello-clone/src/main/resources/db/migration/V5__add_users_table.sql
+++ trello-clone/src/main/resources/db/migration/V5__add_users_table.sql
@@ -3,6 +3,8 @@ create table users
     id           uuid primary key,
     created_when timestamptz not null,
 
+    page_title   varchar(50) not null default 'User Profile',
+
     username     varchar(20) not null unique,
     first_name   varchar(20) not null,
     last_name    varchar(20) not null,
