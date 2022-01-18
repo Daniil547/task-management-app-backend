6104102a336ff16ad903ce6c75e09cd83013cf6a
Daniil547 <72149318+Daniil547@users.noreply.github.com>
2022-01-18 19:22:35 +0200

    Add tests for resource classes
    More concretely to Workspace, Board, CardList, Card, User.

---
+++ trello-clone/src/test/java/io/github/daniil547/board/BoardTest.java
@@ -0,0 +1,126 @@
+package io.github.daniil547.board;
+
+
+import io.github.daniil547.BaseTest;
+import io.github.daniil547.workspace.Workspace;
+import io.github.daniil547.workspace.WorkspaceTest;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Order(2)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class BoardTest extends BaseTest {
+    public static final DefaultBoardService BOARD_SERVICE = new DefaultBoardService(
+            new BoardJdbcRepository(dataSource)
+    );
+
+    private static final String PAGE_NAME = "board123";
+    private static final String BOARD_TITLE = "Smth Board";
+    private static Board board;
+
+    private static final UUID DUMMY_ID = UUID.fromString("123e4567-e89b-12d3-a456-434414174961");
+
+    private static Workspace workspace;
+
+
+    @Test
+    @Order(1)
+    public void createNew() {
+        workspace = WorkspaceTest.WORKSPACE_SERVICE.create("a", "b", "c");
+
+        board = BOARD_SERVICE.create(PAGE_NAME, BOARD_TITLE, "", workspace.getId());
+
+        assertAll(
+                () -> assertNotNull(board),
+                () -> assertEquals(PAGE_NAME, board.getPageName()),
+                () -> assertEquals(BOARD_TITLE, board.getTitle()),
+                () -> assertEquals(BoardVisibility.PRIVATE, board.getVisibility())
+                 );
+    }
+
+    @Test
+    @Order(2)
+    public void createConflicting() {
+        assertThrows(IllegalStateException.class,
+                     () -> BOARD_SERVICE.create(PAGE_NAME, BOARD_TITLE, "", workspace.getId()));
+    }
+
+    @Test
+    @Order(3)
+    public void updateExisting() {
+        String newPageName = "newname987";
+        String newTitle = "New Name";
+        String newDescr = "somethingSomethingNew";
+        BoardVisibility newVisibility = BoardVisibility.PUBLIC;
+
+
+        board.setPageName(newPageName);
+        board.setTitle(newTitle);
+        board.setDescription(newDescr);
+        board.setVisibility(newVisibility);
+
+        board = BOARD_SERVICE.update(board);
+
+        assertAll(
+                () -> assertEquals(newPageName, board.getPageName()),
+                () -> assertEquals(newVisibility, board.getVisibility()),
+                () -> assertEquals(newTitle, board.getTitle()),
+                () -> assertEquals(newDescr, board.getDescription())
+                 );
+    }
+
+    @Test
+    @Order(4)
+    public void updateNonExisting() {
+        UUID actualId = board.getId();
+        board.setId(DUMMY_ID);
+
+        assertThrows(IllegalStateException.class, () -> BOARD_SERVICE.update(board));
+
+        board.setId(actualId);
+    }
+
+    @Test
+    @Order(5)
+    public void getExistingById() {
+        assertEquals(board, BOARD_SERVICE.getById(board.getId()).get());
+    }
+
+    @Test
+    @Order(6)
+    public void getNonExistingById() {
+        assertEquals(Optional.empty(), BOARD_SERVICE.getById(DUMMY_ID));
+    }
+
+    @Test
+    @Order(7)
+    public void getAll() {
+        //this is a new one, as we have updated the previous one
+        Board temp = BOARD_SERVICE.create(PAGE_NAME, BOARD_TITLE, "", workspace.getId());
+        List<Board> allUsers = BOARD_SERVICE.getAll();
+        assertAll(
+                () -> assertTrue(allUsers.contains(board)),
+                () -> assertTrue(allUsers.contains(temp))
+                 );
+    }
+
+    @Test
+    @Order(8)
+    public void deleteById() {
+        BOARD_SERVICE.deleteById(board.getId());
+        assertEquals(Optional.empty(), BOARD_SERVICE.getById(board.getId()));
+    }
+}
+
---
+++ trello-clone/src/test/java/io/github/daniil547/workspace/WorkspaceTest.java
@@ -0,0 +1,119 @@
+package io.github.daniil547.workspace;
+
+import io.github.daniil547.BaseTest;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Order(1)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class WorkspaceTest extends BaseTest {
+    public static final DefaultWorkspaceService WORKSPACE_SERVICE = new DefaultWorkspaceService(
+            new WorkspaceJdbcRepository(dataSource)
+    );
+
+    private static final String PAGE_NAME = "workspace123";
+    private static final String WORKSPACE_TITLE = "Smth Workspace";
+    private static final String COMPANY_WEBSITE_URL = "https://example.com";
+    private static Workspace workspace;
+
+    private static final UUID DUMMY_ID = UUID.fromString("123e4567-e89b-12d3-a456-434414174961");
+
+    @Test
+    @Order(1)
+    public void createNew() {
+
+        workspace = WORKSPACE_SERVICE.create(WORKSPACE_TITLE, PAGE_NAME, "");
+
+        assertAll(
+                () -> assertNotNull(workspace),
+                () -> assertEquals(PAGE_NAME, workspace.getPageName()),
+                () -> assertEquals(WORKSPACE_TITLE, workspace.getTitle()),
+                () -> assertEquals(WorkspaceVisibility.PRIVATE, workspace.getVisibility())
+                 );
+    }
+
+    @Test
+    @Order(2)
+    public void createConflicting() {
+        assertThrows(IllegalStateException.class,
+                     () -> WORKSPACE_SERVICE.create(WORKSPACE_TITLE, PAGE_NAME, ""));
+    }
+
+    @Test
+    @Order(3)
+    public void updateExisting() {
+        String newPageName = "newname987";
+        String newTitle = "New Name";
+        String newDescr = "somethingSomethingNew";
+        WorkspaceVisibility newVisibility = WorkspaceVisibility.PUBLIC;
+
+
+        workspace.setPageName(newPageName);
+        workspace.setTitle(newTitle);
+        workspace.setDescription(newDescr);
+        workspace.setVisibility(newVisibility);
+
+        workspace = WORKSPACE_SERVICE.update(workspace);
+
+        assertAll(
+                () -> assertEquals(newPageName, workspace.getPageName()),
+                () -> assertEquals(newVisibility, workspace.getVisibility()),
+                () -> assertEquals(newTitle, workspace.getTitle()),
+                () -> assertEquals(newDescr, workspace.getDescription())
+                 );
+    }
+
+    @Test
+    @Order(4)
+    public void updateNonExisting() {
+        UUID actualId = workspace.getId();
+        workspace.setId(DUMMY_ID);
+
+        assertThrows(IllegalStateException.class, () -> WORKSPACE_SERVICE.update(workspace));
+
+        workspace.setId(actualId);
+    }
+
+    @Test
+    @Order(5)
+    public void getExistingById() {
+        assertEquals(workspace, WORKSPACE_SERVICE.getById(workspace.getId()).get());
+    }
+
+    @Test
+    @Order(6)
+    public void getNonExistingById() {
+        assertEquals(Optional.empty(), WORKSPACE_SERVICE.getById(DUMMY_ID));
+    }
+
+    @Test
+    @Order(7)
+    public void getAll() {
+        //this is a new one, as we have updated the previous one
+        Workspace temp = WORKSPACE_SERVICE.create(WORKSPACE_TITLE, PAGE_NAME, "");
+        List<Workspace> allUsers = WORKSPACE_SERVICE.getAll();
+        assertAll(
+                () -> assertTrue(allUsers.contains(workspace)),
+                () -> assertTrue(allUsers.contains(temp))
+                 );
+    }
+
+    @Test
+    @Order(8)
+    public void deleteById() {
+        WORKSPACE_SERVICE.deleteById(workspace.getId());
+        assertEquals(Optional.empty(), WORKSPACE_SERVICE.getById(workspace.getId()));
+    }
+}
