0e32b0ddc51b200b0b35bc286dc030c0c1ad72aa
Daniil547 <72149318+Daniil547@users.noreply.github.com>
2022-01-26 17:07:03 +0200

    Migrate to Spring Boot
    Change Main.java according to Spring Boot requirements.
    Remove redundant bean declarations from Config.java.
    Replace application.properties with application.yml.

--- trello-clone/src/main/java/io/github/daniil547/Main.java
+++ trello-clone/src/main/java/io/github/daniil547/Main.java
@@ -1,12 +1,11 @@
 package io.github.daniil547;
 
-import io.github.daniil547.common.db.Migrator;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class, Migrator.class);
-
-        context.close();
+        SpringApplication.run(Main.class, args);
     }
 }
